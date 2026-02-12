5862edd275d426ad9976eb1f5f018aba6b8aea9d
do-alisa <alisa.do@sjsu.edu>
2026-02-11 23:57:52 -0800

    Revert "built blend engine"
    This reverts commit 3c348937ab7a0e2f36ca47fc759e5dbb7c0c77cf.
    
    oopsies

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,19 +1,10 @@
 import 'dotenv/config';
 import express from 'express';
 import cors from 'cors';
-import { generateBlend } from "./blendEngine/generateBlend";
-import type { BlendInput } from "./blendEngine/types";
 
 const app = express();
-app.use(express.json()); // IMPORTANT: lets you read req.body
 const PORT = process.env.PORT || 3001;
 const CLIENT_URL = process.env.CLIENT_URL || 'http://localhost:5173';
-
-app.post("/blend", (req, res) => {
-  const input = req.body as BlendInput;
-  const result = generateBlend(input, 40);
-  res.json(result);
-});
 
 // Middleware
 app.use(cors({ origin: CLIENT_URL }));
